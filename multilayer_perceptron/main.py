57222ebca6308c4001ea4172fd7724d43678fa03
Tsunghao <tsunghao.chen12@gmail.com>
2025-10-08 12:06:39 +0200

    build: fine-tune ruff.toml config

--- multilayer_perceptron/main.py
+++ multilayer_perceptron/main.py
@@ -1,5 +1,15 @@
+import math
+import os
+
+
 def main():
+    """
+    This is a sample docstring.
+    """
     print("Hello from multilayer-perceptron!")
+    path = os.getcwd()
+    nan = math.nan
+    print(f"I am at {path}, this is nan {nan} LKJLKJLKJLKJLKJLKJLKJLKJLKJLK")
 
 
 if __name__ == "__main__":
